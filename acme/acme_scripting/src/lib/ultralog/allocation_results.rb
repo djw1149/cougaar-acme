a0fb727b799faf8c65e7de7206358188658fdcad
rich <rich>
2003-02-21 14:28:40 +0000

    This is a transfer from a file Mark Barger is working on...initial import.

---
+++ acme/acme_scripting/src/lib/ultralog/allocation_results.rb
@@ -0,0 +1,45 @@
+if $0 == __FILE__
+  $:.unshift ".."
+  require 'cougaar/society_model'
+end
+
+require 'uri'
+require 'net/http'
+require 'ultralog/aggagent'
+require 'cougaar/communications'
+
+module Cougaar
+
+class AllocationResults
+
+    def initialize(host, agent, port=8800)
+      if host.kind_of? Cougaar::Society
+        society=host
+        host = society.agents['AGG-Agent'].node.host.host_name
+      end
+      @host  = host
+      @port  = port
+      @agent = agent
+      @uri = "http://#{@host}:#{@port}/$#{@agent}/AllocationResults"
+      puts @uri+'<--URI'
+    end
+
+    def query
+      response, url = Cougaar::Util.do_http_request(@uri);
+      return response;
+      rescue
+        result = "<AllocationResultsException>\n"
+        result += $!
+        result += "\n"
+        result += $!.backtrace.join("\n")
+        result += "\n</AllocationResultsException>"
+        return result
+    end
+
+end # class AllocationResults
+
+end # module
+
+if __FILE__ == $0
+  puts Cougaar::AllocationResults.new('192.168.120.214', '1-501-AVNBN', 8800).query
+end
