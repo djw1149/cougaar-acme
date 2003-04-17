6357ed9de51ee4395dba989a5532dca076a0d1d1
berliner <berliner>
2003-04-17 01:31:14 +0000

    Shell scripts for creating 6 *.facets.xml societies and 6 *.plugins.xml societies.

---
+++ acme/config/org_agent_builder/make-facet-societies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Make seven facet societies
+echo "Making single-1ad.facets.xml..."
+ruby org_agent_builder.rb -m single-1ad.member.csv -s 1ad/single-1ad.facets.xml
+echo "Done"
+echo "Making tiny-1ad.facets.xml..."
+ruby org_agent_builder.rb -m tiny-1ad.member.csv -s 1ad/tiny-1ad.facets.xml
+echo "Done"
+echo "Making tiny-1ad-trans-stub.facets.xml..."
+ruby org_agent_builder.rb -m tiny-1ad-trans-stub.member.csv -s 1ad/tiny-1ad-trans-stub.facets.xml
+echo "Done"
+echo "Making small-1ad.facets.xml..."
+ruby org_agent_builder.rb -m small-1ad.member.csv -s 1ad/small-1ad.facets.xml
+echo "Done"
+echo "Making small-1ad-trans-stub.facets.xml..."
+ruby org_agent_builder.rb -m small-1ad-trans-stub.member.csv -s 1ad/small-1ad-trans-stub.facets.xml
+echo "Done"
+echo "Making full-1ad.facets.xml..."
+ruby org_agent_builder.rb -m full-1ad.member.csv -s 1ad/full-1ad.facets.xml
+echo "Done"
+echo "Making full-1ad-trans-stub.facets.xml..."
+ruby org_agent_builder.rb -m full-1ad-trans-stub.member.csv -s 1ad/full-1ad-trans-stub.facets.xml
+echo "Done"
---
+++ acme/config/org_agent_builder/make-plugin-societies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Make seven plugin societies
+echo "Making single-1ad.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/single-1ad.facets.xml -r base_rules -o 1ad/single-1ad.plugins.xml
+echo "Done"
+echo "Making tiny-1ad.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/tiny-1ad.facets.xml -r base_rules -o 1ad/tiny-1ad.plugins.xml
+echo "Done"
+echo "Making tiny-1ad-trans-stub.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/tiny-1ad-trans-stub.facets.xml -r base_rules -o 1ad/tiny-1ad-trans-stub.plugins.xml
+echo "Done"
+echo "Making small-1ad.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/small-1ad.facets.xml -r base_rules -o 1ad/small-1ad.plugins.xml
+echo "Done"
+echo "Making small-1ad-trans-stub.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/small-1ad-trans-stub.facets.xml -r base_rules -o 1ad/small-1ad-trans-stub.plugins.xml
+echo "Done"
+echo "Making full-1ad.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/full-1ad.facets.xml -r base_rules -o 1ad/full-1ad.plugins.xml
+echo "Done"
+echo "Making full-1ad-trans-stub.plugins.xml..."
+ruby ../bin/transform_society.rb -i 1ad/full-1ad-trans-stub.facets.xml -r base_rules -o 1ad/full-1ad-trans-stub.plugins.xml
+echo "Done"
