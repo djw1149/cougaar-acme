f5a28318d29996fc781230fc47fd4c7ed9d16aab
rich <rich>
2003-05-13 15:49:57 +0000

    added the ability to layout during transform and fixed nameserver rule

--- acme/config/bin/transform_society.rb
+++ acme/config/bin/transform_society.rb
@@ -13,12 +13,16 @@ require 'cougaar/scripting'
 require 'getoptlong'
 
 opts = GetoptLong.new( [ '--input',	'-i',		GetoptLong::REQUIRED_ARGUMENT],
+                      [ '--layout', '-l', GetoptLong::REQUIRED_ARGUMENT],
+                      [ '--hosts', '-h', GetoptLong::REQUIRED_ARGUMENT],
 											[ '--rules', '-r', GetoptLong::REQUIRED_ARGUMENT ],
 											[ '--output', '-o', GetoptLong::REQUIRED_ARGUMENT ],
                       [ '--abort-on-warning', '-a',  GetoptLong::NO_ARGUMENT],
-											[ '--help', '-h', GetoptLong::NO_ARGUMENT])
+											[ '--help', '-?', GetoptLong::NO_ARGUMENT])
 
 input = nil
+layout = nil
+hosts = nil
 output = nil
 input_type = :unknown
 output_type = :unknown
@@ -26,8 +30,10 @@ abort_on_warning = false
 rules = nil
 
 def help
-  puts "Transforms a society with rules (and converts between xml and ruby).\nUsage:\n\t#$0 -i <input file> -r <rules dir> [-o <output file>] [-h]"
+  puts "Transforms a society with rules (and converts between xml and ruby).\nUsage:\n\t#$0 -i <input file> -l <layout file> [-h <hosts file>] -r <rules dir> [-o <output file>] [-?]"
   puts "\t-i --input\tThe input file (.xml or .rb)."
+  puts "\t-l --layout\tThe layout file (.xml or .rb)."
+  puts "\t-h --hosts\tThe hosts file (.xml or .rb)."
   puts "\t-r --rules\tThe rule directory (e.g. ./rules)."
   puts "\t-a --abort-on-warning\tAbort the generation of the society if a rule warning is encountered."
   puts "\t-o --output\tThe output file. (default new-<input>)"
@@ -39,6 +45,10 @@ opts.each do |opt, arg|
     input = arg
     input_type = :xml if (File.basename(input)!=File.basename(input, ".xml"))
     input_type = :ruby if (File.basename(input)!=File.basename(input, ".rb"))
+  when '--layout'
+    layout = arg
+  when '--hosts'
+    hosts = arg
   when '--rules'
     rules = arg
   when '--output'
@@ -74,6 +84,14 @@ unless File.exist?(input)
   exit
 end
 
+if hosts
+  unless layout
+    puts "Incorrect usage...must supply layout file if using a hosts file.\n"
+    help
+    exit
+  end
+end
+
 # TRANSFORM SOCIETY
 
 print "Loading #{input}..."
@@ -84,16 +102,25 @@ when :ruby
 when :xml
   Cougaar::SocietyBuilder.from_xml_file(input)
 end
-builder.society
+society = builder.society
 puts "done."
+
+if layout
+  print "Laying out society with layout file #{layout}"
+  print " and hosts file #{hosts}" if hosts
+  puts ""
+  sl = Cougaar::Model::SocietyLayout.from_society(society, layout, hosts)
+  sl.layout
+  print "Finished laying out society."
+end
 puts "Applying transformation rules from #{rules}..."
 starttime = Time.now
-engine = Cougaar::Model::RuleEngine.new(builder.society)
+engine = Cougaar::Model::RuleEngine.new(society)
 engine.abort_on_warning = abort_on_warning
 engine.enable_stdout
 engine.load_rules(rules)
 engine.execute
-puts "finished in #{Time.now - starttime} seconds."
+puts "Finished in #{Time.now - starttime} seconds."
 puts "Writing #{output}..."
 $stdout.flush
 case output_type
@@ -102,4 +129,4 @@ when :ruby
 when :xml
   builder.to_xml_file(output)
 end
-puts "done."
+puts "Done."
