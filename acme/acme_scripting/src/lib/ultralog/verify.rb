c8d4af351b45a6b0ac0bd7512aecd8c84490b42c
jwong <jwong>
2003-05-19 13:58:54 +0000

    Added SanityCheck - Use it like this:   wait_for "SanityCheck", "123-MSB", 1000

---
+++ acme/acme_scripting/src/lib/ultralog/verify.rb
@@ -0,0 +1,43 @@
+require 'cougaar/scripting'
+require 'net/http'
+
+module Cougaar
+  module States
+
+  class SanityCheck < Cougaar::State
+    DOCUMENTATION = Cougaar.document {
+      @description = "Verify that agents have completed a certain number of tasks."
+    }
+
+    def initialize(run, agent, tasks, timeout=nil, &block)
+       super(run, timeout, &block)
+       @agent = agent
+       @tasks = tasks
+    end
+
+    def process
+      host = @run.society.agents[@agent].node.host
+      port = 8800
+      tasks = 0
+
+      server = Net::HTTP.new( host.host_name, port )
+      resp, data = server.get("/$#{@agent}/completion?format=xml");
+      doc = REXML::Document.new data
+      doc.elements.each("SimpleCompletion/NumTasks") {
+         |element|
+         tasks = element.text.to_i         
+      }
+
+      if (tasks < @tasks) 
+        handle_timeout
+        @sequence.interrupt
+      end
+    end
+
+    def unhandled_timeout
+      @run.do_action "StopSociety"
+      @run.do_action "StopCommunications"
+    end
+  end
+  end
+end
