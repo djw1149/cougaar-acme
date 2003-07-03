31396ddba368cc728f4cbb50313cd7b63bcaeb1c
rich <rich>
2003-07-03 13:16:14 +0000

    added rexec_user command which executes as the acme_config user and made this plugin dependent on the acme_cougaar_config plugin

--- acme/acme_service/src/plugins/acme_host_rexec/rexec.rb
+++ acme/acme_service/src/plugins/acme_host_rexec/rexec.rb
@@ -11,12 +11,22 @@ module ACME; module Plugins
     attr_reader :plugin
     def initialize(plugin)
       @plugin = plugin
+      @config_mgr = plugin['/cougaar/config'].manager      
       
       @plugin["/plugins/acme_host_jabber_service/commands/rexec/description"].data = 
         "Executes host command. Params: host_command"
       @plugin["/plugins/acme_host_jabber_service/commands/rexec"].set_proc do |message, command| 
         status = "\n"
         command = command.gsub(/\&quot;/, '"').gsub(/\&apos;/, "'")
+        status << `#{command}`.gsub(/\&/, "&amp;").gsub(/\</, "&lt;")
+        message.reply.set_body(status).send
+      end
+      @plugin["/plugins/acme_host_jabber_service/commands/rexec_user/description"].data = 
+        "Executes host command as the acme_config user. Params: host_command"
+      @plugin["/plugins/acme_host_jabber_service/commands/rexec_user"].set_proc do |message, command| 
+        status = "\n"
+        command = command.gsub(/\&quot;/, '"').gsub(/\&apos;/, "'")
+        command = @config_mgr.cmd_wrap(command)
         status << `#{command}`.gsub(/\&/, "&amp;").gsub(/\</, "&lt;")
         message.reply.set_body(status).send
       end
