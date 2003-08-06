e2c1c83981bed5d5bd0b86423021cfadbfc46e0f
bkrisler <bkrisler>
2003-08-06 21:03:02 +0000

    Fixed paths to rule sort.

--- acme/config/org_agent_builder/gen-plugins-xml.sh
+++ acme/config/org_agent_builder/gen-plugins-xml.sh
@@ -2,14 +2,14 @@
 
 if [ "x$1" = "x" ]; then
   echo "Usage: $0 <partial relative path to facets file>"
-  echo "    IE: 1ua/17a11v.oo arg produces 1ua/17a11v.oo.plugins.xml from"
-  echo "        1ua/17a11v.oo.facets.xml"
+  echo "    IE: 1ua/17a11v arg produces 1ua/17a11v.plugins.xml from"
+  echo "        17a11v.facets.xml"
   exit
 fi
 
 # First put the rules in order
 echo "Sorting the rules and writing to base_rules/temp..."
-./rule.sort.sh base_rules
+./rule.sort.sh base_rules ../rules/metrics
 ls base_rules/temp
 echo "Done"
 echo "Making $1.plugins.xml..."
