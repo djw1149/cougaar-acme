b1daa510f605ff28bedb09b5450ad6108f9059ac
bkrisler <bkrisler>
2003-08-07 14:10:17 +0000

    Changed directory from ul to ad

--- acme/config/org_agent_builder/create-ad-dist.sh
+++ acme/config/org_agent_builder/create-ad-dist.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 # This script creates a UL distro package of new 1AD societies.
 
-ULDIR=../societies/ul
+ADDIR=../societies/ad
 
-if [ ! -d $ULDIR ]; then
-    mkdir $ULDIR
+if [ ! -d $ADDIR ]; then
+    mkdir $ADDIR
 fi
 
 echo "Making All the facet files"
-ruby org_agent_builder.rb -d 1ad.org_data -m tiny-1ad.member.csv -s ../societies/ul/tiny-1ad.oo.facets.xml -o
-ruby org_agent_builder.rb -d 1ad.org_data -m tiny-1ad-trans-stub.member.csv -s ../societies/ul/tiny-1ad-trans-stub.oo.facets.xml -o
-ruby org_agent_builder.rb -d 1ad.org_data -m small-1ad.member.csv -s ../societies/ul/small-1ad.oo.facets.xml -o
-ruby org_agent_builder.rb -d 1ad.org_data -m small-1ad-trans-stub.member.csv -s ../societies/ul/small-1ad-trans-stub.oo.facets.xml -o
-ruby org_agent_builder.rb -d 1ad.org_data -m full-1ad.member.csv -s ../societies/ul/full-1ad.oo.facets.xml -o
-ruby org_agent_builder.rb -d 1ad.org_data -m full-1ad-trans-stub.member.csv -s ../societies/ul/full-1ad-trans-stub.oo.facets.xml -o
+ruby org_agent_builder.rb -d 1ad.org_data -m tiny-1ad.member.csv -s $ADDIR/tiny-1ad.oo.facets.xml -o
+ruby org_agent_builder.rb -d 1ad.org_data -m tiny-1ad-trans-stub.member.csv -s $ADDIR/tiny-1ad-trans-stub.oo.facets.xml -o
+ruby org_agent_builder.rb -d 1ad.org_data -m small-1ad.member.csv -s $ADDIR/small-1ad.oo.facets.xml -o
+ruby org_agent_builder.rb -d 1ad.org_data -m small-1ad-trans-stub.member.csv -s $ADDIR/small-1ad-trans-stub.oo.facets.xml -o
+ruby org_agent_builder.rb -d 1ad.org_data -m full-1ad.member.csv -s $ADDIR/full-1ad.oo.facets.xml -o
+ruby org_agent_builder.rb -d 1ad.org_data -m full-1ad-trans-stub.member.csv -s $ADDIR/full-1ad-trans-stub.oo.facets.xml -o
 ## Currently the ammo facets is generated in the SocietyDesigner (until org_agent_builder is fixed).  So copy it.
-cp ./1ad.orig-org-id/small-1ad-ammo.facets.xml ../societies/ul/
+cp ./1ad.orig-org-id/small-1ad-ammo.facets.xml $ADDIR/
 echo "Done making facets"
 
 echo "Sorting the rules and writing to base_rules/temp..."
@@ -23,29 +23,29 @@ echo "Sorting the rules and writing to base_rules/temp..."
 echo "Done"
 
 echo "Making the societies"
-ruby ../bin/transform_society.rb -i $ULDIR/tiny-1ad.oo.facets.xml -r base_rules/temp -o $ULDIR/TINY-TRANS.xml
-ruby ../bin/transform_society.rb -i $ULDIR/tiny-1ad-trans-stub.oo.facets.xml -r base_rules/temp -o $ULDIR/TINY-TRANS-STUB.xml
-ruby ../bin/transform_society.rb -i $ULDIR/small-1ad.oo.facets.xml -r base_rules/temp -o $ULDIR/SMALL-TRANS.xml
-ruby ../bin/transform_society.rb -i $ULDIR/small-1ad-trans-stub.oo.facets.xml -r base_rules/temp -o $ULDIR/SMALL-TRANS-STUB.xml
-ruby ../bin/transform_society.rb -i $ULDIR/small-1ad-ammo.facets.xml -r base_rules/temp -o $ULDIR/SMALL-TRANS-AMMO.xml
-ruby ../bin/transform_society.rb -i $ULDIR/full-1ad.oo.facets.xml -r base_rules/temp -o $ULDIR/FULL-TRANS.xml
-ruby ../bin/transform_society.rb -i $ULDIR/full-1ad-trans-stub.oo.facets.xml -r base_rules/temp -o $ULDIR/FULL-TRANS-STUB.xml
+ruby ../bin/transform_society.rb -i $ADDIR/tiny-1ad.oo.facets.xml -r base_rules/temp -o $ADDIR/TINY-TRANS.xml
+ruby ../bin/transform_society.rb -i $ADDIR/tiny-1ad-trans-stub.oo.facets.xml -r base_rules/temp -o $ADDIR/TINY-TRANS-STUB.xml
+ruby ../bin/transform_society.rb -i $ADDIR/small-1ad.oo.facets.xml -r base_rules/temp -o $ADDIR/SMALL-TRANS.xml
+ruby ../bin/transform_society.rb -i $ADDIR/small-1ad-trans-stub.oo.facets.xml -r base_rules/temp -o $ADDIR/SMALL-TRANS-STUB.xml
+ruby ../bin/transform_society.rb -i $ADDIR/small-1ad-ammo.facets.xml -r base_rules/temp -o $ADDIR/SMALL-TRANS-AMMO.xml
+ruby ../bin/transform_society.rb -i $ADDIR/full-1ad.oo.facets.xml -r base_rules/temp -o $ADDIR/FULL-TRANS.xml
+ruby ../bin/transform_society.rb -i $ADDIR/full-1ad-trans-stub.oo.facets.xml -r base_rules/temp -o $ADDIR/FULL-TRANS-STUB.xml
 echo "Done"
 
 echo "Fix up the node names"
-sed 's/localnode/1AD_TINY/' $ULDIR/TINY-TRANS.xml > $ULDIR/TINY-1AD-TRANS-1359.xml
-sed 's/localnode/1AD_TINY/' $ULDIR/TINY-TRANS-STUB.xml > $ULDIR/TINY-1AD-TRANS-STUB-1359.xml
-sed 's/localnode/SMALL_1AD/' $ULDIR/SMALL-TRANS.xml > $ULDIR/SMALL-1AD-TRANS-1359.xml
-sed 's/localnode/SMALL_1AD/' $ULDIR/SMALL-TRANS-STUB.xml > $ULDIR/SMALL-1AD-TRANS-STUB-1359.xml
-sed 's/localnode/SMALL_1AD/' $ULDIR/SMALL-TRANS-AMMO.xml > $ULDIR/SMALL-1AD-TRANS-5.xml
-sed 's/localnode/FULL_1AD/' $ULDIR/FULL-TRANS.xml > $ULDIR/FULL-1AD-TRANS-1359.xml
-sed 's/localnode/FULL_1AD/' $ULDIR/FULL-TRANS-STUB.xml > $ULDIR/FULL-1AD-TRANS-STUB-1359.xml
+sed 's/localnode/1AD_TINY/' $ADDIR/TINY-TRANS.xml > $ADDIR/TINY-1AD-TRANS-1359.xml
+sed 's/localnode/1AD_TINY/' $ADDIR/TINY-TRANS-STUB.xml > $ADDIR/TINY-1AD-TRANS-STUB-1359.xml
+sed 's/localnode/SMALL_1AD/' $ADDIR/SMALL-TRANS.xml > $ADDIR/SMALL-1AD-TRANS-1359.xml
+sed 's/localnode/SMALL_1AD/' $ADDIR/SMALL-TRANS-STUB.xml > $ADDIR/SMALL-1AD-TRANS-STUB-1359.xml
+sed 's/localnode/SMALL_1AD/' $ADDIR/SMALL-TRANS-AMMO.xml > $ADDIR/SMALL-1AD-TRANS-5.xml
+sed 's/localnode/FULL_1AD/' $ADDIR/FULL-TRANS.xml > $ADDIR/FULL-1AD-TRANS-1359.xml
+sed 's/localnode/FULL_1AD/' $ADDIR/FULL-TRANS-STUB.xml > $ADDIR/FULL-1AD-TRANS-STUB-1359.xml
 echo "Done"
 
 echo "Create the zip package"
-zip -j $ULDIR/1ad-configs.zip $ULDIR/*-*5*.xml 
+zip -j $ADDIR/1ad-configs.zip $ADDIR/*-*5*.xml 
 echo "Done"
 
 echo "Clean up the mess we made"
-rm $ULDIR/*.xml
+rm $ADDIR/*.xml
 echo "Done cleaning up"
