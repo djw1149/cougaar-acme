03d40814a2170bb78830f05be3b61e7818424d87
ahelsing <ahelsing>
2003-10-03 22:57:04 +0000

    Comment back out the change to how agents were moved -- it caused funny WARN/ERRORs in the logs, although the moves were still succesful. And besides, the problem is pretty rare.

--- acme/acme_scripting/src/lib/cougaar/society_control.rb
+++ acme/acme_scripting/src/lib/cougaar/society_control.rb
@@ -417,8 +417,10 @@ module Cougaar
 	    @run.info_message "No agent #{@agent} in society to move to #{@node}!"
 	  else
 	    # Could (should?) also check if the agent is already on the named node
-	    # Note we ask the Node to move the agent, to avoid any timing issues
-	    uri = "#{@run.society.agents[@agent].node.uri}/move?op=Move&mobileAgent=#{@agent}&originNode=&destNode=#{@node}&isForceRestart=false&action=Add"
+	    # Note we could ask the Node to move the agent, to avoid any timing issues. But that
+	    # seems to leave ugly WARNs and ERRORs in the logs that have no real harm
+#	    uri = "#{@run.society.agents[@agent].node.uri}/move?op=Move&mobileAgent=#{@agent}&originNode=&destNode=#{@node}&isForceRestart=false&action=Add"
+	    uri = "#{@run.society.agents[@agent].uri}/move?op=Move&mobileAgent=#{@agent}&originNode=&destNode=#{@node}&isForceRestart=false&action=Add"
 	    result = Cougaar::Communications::HTTP.get(uri)
 	    unless result
 	      @run.error_message "Error moving agent #{@agent} using uri #{uri}" 
