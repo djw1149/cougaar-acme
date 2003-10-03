f68781858119d74c11449a286c68931c0a2a8681
mbarger <mbarger>
2003-10-03 21:30:54 +0000

    remove killall of java processes - now the CleanupSociety action will restart acme services so that we know they're in a good state before every run.

--- acme/acme_service/src/acme.sh
+++ acme/acme_service/src/acme.sh
@@ -1,9 +1,6 @@
 #!/bin/csh
 
 while (1)
-  # ACME won't start properly if Cougaar node is running, because that keeps event
-  # port open.
-  killall -9 java
   /usr/bin/ruby acme.rb >>& run.log &
   echo "$!" > acme.pid
   while (-e /proc/$!) 
