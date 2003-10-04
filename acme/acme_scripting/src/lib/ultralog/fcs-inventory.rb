30cbce1b31bf72640b2312849e93e1ab048ecc54
rich <rich>
2003-10-04 15:58:37 +0000

    tag file for archive and remove

--- acme/acme_scripting/src/lib/ultralog/fcs-inventory.rb
+++ acme/acme_scripting/src/lib/ultralog/fcs-inventory.rb
@@ -41,6 +41,7 @@ module Cougaar
         File.open(@file, "wb") do |file|
           file.puts result
         end
+        @run.archive_and_remove_file(@file, "FCS Inventory for agent: #{@agent} asset: #{@asset}")
       end
 
       def perform
