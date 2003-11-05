c3ad12b74dbe921cff21c3bcd72b0e4fc65e7ec0
art <art>
2003-11-05 17:17:38 +0000

    trying to fix typos so we actually get inventory charts

--- acme/acme_scripting/src/lib/ultralog/ua-inventory.rb
+++ acme/acme_scripting/src/lib/ultralog/ua-inventory.rb
@@ -40,10 +40,10 @@ module Cougaar
         # Get some inventory charts
         get_inventory  "1-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0", "JP8:NSN/9130010315816:1-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0-FCS-MCS-1", "INV/INV#{@modifier}-1-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0.xml"
         get_inventory  "1-CA-BN-MCS-CO-A-1-PLT-FCS-ARV-R-0", "JP8:NSN/9130010315816:1-CA-BN-MCS-CO-A-1-PLT-FCS-ARV-R-0-FCS-ARV-R-1", "INV/INV#{@modifier}-1-CA-BN-MCS-CO-A-1-PLT-FCS-ARV-R-0.xml"
-        get_inventory  "1-CA-BN-RECON-DET-1-PLT-FCS-ARV-R-0", "JP8:NSN/9130010315816:1-CA-BN-RECON-DET-1-PLT-FCS-ARV-R-1", "INV/INV#{@modifier}-1-CA-BN-RECON-DET-1-PLT-FCS-ARV-R-0.xml"
-        get_inventory  "2-CA-BN-INF-CO-A-1-PLT-FCS-ICV-1", "JP8:NSN/9130010315816:2-CA-BN-INF-CO-A-1-PLT-FCS-ICV-1", "INV/INV#{@modifier}-2-CA-BN-INF-CO-A-1-PLT-FCS-ICV-1.xml"
-        get_inventory  "2-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0", "JP8:NSN/9130010315816:2-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-1", "INV/INV#{@modifier}2-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0.xml"
-        get_inventory  "UA-MCG2-FCS-ICV-0", "JP8:NSN/9130010315816:UA-MCG2-FCS-ICV-1", "INV/INV#{@modifier}UA-MCG2-FCS-ICV-0.xml"
+        get_inventory  "1-CA-BN-RECON-DET-1-PLT-FCS-ARV-R-0", "JP8:NSN/9130010315816:1-CA-BN-RECON-DET-1-PLT-FCS-ARV-R-0-ARV-R-1", "INV/INV#{@modifier}-1-CA-BN-RECON-DET-1-PLT-FCS-ARV-R-0.xml"
+        get_inventory  "2-CA-BN-INF-CO-A-1-PLT-FCS-ICV-1", "JP8:NSN/9130010315816:2-CA-BN-INF-CO-A-1-PLT-FCS-ICV-1-FCS-ICV-1", "INV/INV#{@modifier}-2-CA-BN-INF-CO-A-1-PLT-FCS-ICV-1.xml"
+        get_inventory  "2-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0", "JP8:NSN/9130010315816:2-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0-FCS-MCS-1", "INV/INV#{@modifier}2-CA-BN-MCS-CO-A-1-PLT-FCS-MCS-0.xml"
+        get_inventory  "UA-MCG2-FCS-ICV-0", "JP8:NSN/9130010315816:UA-MCG2-FCS-ICV-0-FCS-ICV-1", "INV/INV#{@modifier}UA-MCG2-FCS-ICV-0.xml"
 
       end
       
@@ -62,45 +62,6 @@ module Cougaar
           end
         else
           @run.error_message "Inventory failed, unknown agent: #{agent}"
-        end
-      end
-    end
-  
-    # Sample the inventory of a society in predefined places
-    class SampleInventory < Cougaar::Action
-      PRIOR_STATES = ["SocietyRunning"]
-
-      attr_accessor :asset, :agent, :protocol, :society, :runport, :runhost
-
-      # Take the asset to get the inventory for at this agent
-      def initialize(run, agent, asset, file)
-        super(run)
-          @file = file
-          @asset = asset
-          @agent = agent
-          @protocol = "http" 
-      end
-
-      def save(result)
-        File.open(@file, "wb") do |file|
-          file.puts result
-        end
-        @run.archive_and_remove_file(@file, "Inventory for agent: #{@agent} asset: #{@asset}")
-      end
-
-      def perform
-        cougaar_agent = @run.society.agents[@agent]
-        if cougaar_agent
-          list, uri = Cougaar::Communications::HTTP.get("#{cougaar_agent.uri}/list")
-          if uri
-            #puts "SampleInventory: About to do put to: #{uri.scheme}://#{uri.host}:#{uri.port}/$#{@agent}/log_inventory for #{@asset}"
-            resp = Cougaar::Communications::HTTP.put("#{uri.scheme}://#{uri.host}:#{uri.port}/$#{@agent}/log_inventory", @asset)
-            save(resp)
-          else
-            @run.error_message "Inventory failed to redirect to agent: #{@agent}"
-          end
-        else
-          @run.error_message "Inventory failed, unknown agent: #{@agent}"
         end
       end
     end
