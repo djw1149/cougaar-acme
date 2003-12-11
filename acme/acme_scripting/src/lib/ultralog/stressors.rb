9a72a2def735b39afb686483f3bd3f5fae7ed389
jnilsson <jnilsson>
2003-12-11 17:22:43 +0000

    Added actions EnableMemWasterLogging and DisableMemWasterLogging Use these actions to enable/disable memory waster logging by node. If no node is specified then logging is enabled/disabled on all nodes.

--- acme/acme_scripting/src/lib/ultralog/stressors.rb
+++ acme/acme_scripting/src/lib/ultralog/stressors.rb
@@ -49,6 +49,60 @@ module Cougaar
       end
     end
     
+    class DisableMemWasterLogging < Cougaar::Action
+      PRIOR_STATES = ["SocietyRunning"]
+    
+      # Take the asset to get the inventory for at this agent
+      def initialize(run, *nodes)
+        super(run)
+        @nodes = nodes
+      end
+    
+      def perform
+	if @nodes.empty? then
+	  @run.society.each_node do |cougaar_node|
+            data, uri = Cougaar::Communications::HTTP.get("#{cougaar_node.uri}/$#{cougaar_node.name}/mem-waster?log=disable")
+          end
+        else
+          @nodes.each do |nodename|
+            cougaar_node = @run.society.nodes[nodename]
+            if cougaar_node
+              data, uri = Cougaar::Communications::HTTP.get("#{cougaar_node.uri}/$#{nodename}/mem-waster?log=disable")
+            else
+              @run.error_message "DisableMemWasterLogging Could not find node: #{nodename}"
+            end
+          end
+        end
+      end
+    end 
+
+   class EnableMemWasterLogging < Cougaar::Action
+      PRIOR_STATES = ["SocietyRunning"]
+    
+      # Take the asset to get the inventory for at this agent
+      def initialize(run, *nodes)
+        super(run)
+        @nodes = nodes
+      end
+    
+      def perform
+	if @nodes.empty? then
+	  @run.society.each_node do |cougaar_node|
+            data, uri = Cougaar::Communications::HTTP.get("#{cougaar_node.uri}/$#{cougaar_node.name}/mem-waster?log=enable")
+	  end
+        else
+          @nodes.each do |nodename|
+            cougaar_node = @run.society.nodes[nodename]
+            if cougaar_node
+              data, uri = Cougaar::Communications::HTTP.get("#{cougaar_node.uri}/$#{nodename}/mem-waster?log=enable")
+            else
+              @run.error_message "EnableMemWasterLogging Could not find node: #{nodename}"
+            end
+          end
+        end
+      end
+    end 
+
     class EnableNetworkInterfaces < Cougaar::Action
       PRIOR_STATES = ["SocietyLoaded"]
       DOCUMENTATION = Cougaar.document {
