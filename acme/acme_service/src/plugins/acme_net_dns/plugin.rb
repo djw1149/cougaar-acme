cc64e76f95a5502af738792ce5b12020993b44fd
dpeugh <dpeugh>
2004-03-30 22:51:42 +0000

    Initial version of DNS controls for ACME.  Simple interface:    move( hostname, ipaddress ) -- Changes the IP address handed out       by DNS.  Returns the new address.    lookup( hostname ) -- Determines the IP address being given out by       the DNS server.    reset -- Reset DNS to a predefined setting.

---
+++ acme/acme_service/src/plugins/acme_net_dns/plugin.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/ruby
+
+#
+#
+
+module ACME; module Plugins
+
+class DNS
+  extend FreeBASE::StandardPlugin
+
+  def self.start(plugin)
+    plugin['log/info'] << "ACME::Plugin::DNS[start]"
+    plugin['instance'] = DNS.new( plugin )
+
+    plugin.transition(FreeBASE::RUNNING)
+  end
+
+  def self.stop(plugin)
+    plugin['log/info'] << "ACME::Plugin::DNS[stop]"
+    plugin.do_reset
+
+    plugin.transition(FreeBASE::LOADED)
+  end
+
+  attr_reader :plugin
+
+  def initialize( plugin )
+    super( )
+    @plugin = plugin
+    @plugin['log/info'] << "Initializing acme_net_dns"
+
+    cmd = @plugin.properties["command"]
+    desc = @plugin.properties["description"]
+    
+    @zones = @plugin.properties["zone"]
+    @backup = @plugin.properties["zone.bk"]
+
+    @plugin["/plugins/acme_host_communications/commands/#{cmd}/description"].data = desc
+    @plugin["/plugins/acme_host_communications/commands/#{cmd}"].set_proc do |msg, cmd|
+       case cmd
+         # move( hostname, ipaddress ) - This method change the IP Address
+         # associated with the given hostname.
+
+         when /move\((.*),(.*)\)/
+           plugin['log/info'] << "Moving host #{$1} to address #{$2}"
+           reply = do_move( $1, $2 )
+
+         # lookup - Returns the IP Address the DNS is handing out.
+         when /lookup\((.*)\)/
+           plugin['log/info'] << "Looking up host #{$1}"
+           reply = do_lookup( $1 )
+        
+         # reset - This method restores DNS to a pristine state
+         when /reset/
+           plugin['log/info'] << "Resetting DNS to a known state."
+           reply = do_reset
+         else 
+           reply = "#{cmd} unknown-#{command}"
+       end
+       msg.reply.set_body( reply ).send
+    end           
+  end
+
+  def do_move( hostname, ipaddress )
+    @plugin['log/info'] << "Changing #{hostname} to #{ipaddress}"
+    do_lookup( hostname )
+  end
+
+  def do_reset
+    @plugin['log/info'] << "Resetting DNS to known state."
+    @plugin['log/info'] << "cp #{@backup} to #{@zones}"
+    `/sbin/service named stop`
+    @plugin['log/info'] << `cp #{@backup} #{@zones}`
+    `/sbin/service named start`
+    `nslookup sv191 -silent | grep Address | grep -v \\# | cut -d: -f2`.strip!
+  end
+
+  def do_lookup( hostname )
+    `nslookup #{hostname} -silent | grep Address | grep -v \\# | cut -d: -f2`.strip!
+  end 
+
+
+end
+
+end; end
