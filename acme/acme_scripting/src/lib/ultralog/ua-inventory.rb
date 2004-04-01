52103f42568bc2880ec25ed627978fb7c66ec209
art <art>
2004-04-01 18:46:53 +0000

    new agents per jeff's e-mail 0n 3/31/04

--- acme/acme_scripting/src/lib/ultralog/ua-inventory.rb
+++ acme/acme_scripting/src/lib/ultralog/ua-inventory.rb
@@ -44,17 +44,42 @@ module Cougaar
 
 
         # Get some inventory charts
-        get_inventory  "FCS-C2V-0.1-PLT-HQ.INFCO-A.1-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-C2V-0.1-PLT-HQ.INFCO-A.1-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}FCS-C2V-0.1-PLT-HQ.INFCO-A.1-CABN.1-UA.ARMY.MIL.xml"
-        get_inventory  "FCS-ARV-A-0.1-PLT-HQ.INFCO-A.1-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ARV-A-0.1-PLT-HQ.INFCO-A.1-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ARV-A-0.1-PLT-HQ.INFCO-A.1-CABN.1-UA.ARMY.MIL.xml"
-        get_inventory  "FCS-ARV-R-0.1-PLT.RECON-DET.1-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ARV-R-0.1-PLT.RECON-DET.1-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ARV-R-0.1-PLT.RECON-DET.1-CABN.1-UA.ARMY.MIL.xml"
-        get_inventory  "FCS-ARV-A-0.1-PLT-HQ.INFCO-A.2-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ARV-A-0.1-PLT-HQ.INFCO-A.2-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}FCS-ARV-A-0.-1-PLT-HQ.INFCO-A.2-CABN.1-UA.ARMY.MIL.xml"
-        get_inventory  "FCS-MCS-0.2-PLT.MCSCO-B.2-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-MCS-0.2-PLT.MCSCO-B.2-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-MCS-0.2-PLT.MCSCO-B.2-CABN.1-UA.ARMY.MIL.xml"
-        get_inventory  "FCS-ICV-0.MCG1.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ICV-0.MCG1.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ICV-0.MCG1.1-UA.ARMY.MIL-FCS-ICV-0.xml"
+        get_inventory  "FCS-ICV-0.2-PLT-3-INF-SQUAD.INFCO-A.3-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ICV-0.2-PLT-3-INF-SQUAD.INFCO-A.3-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ICV-0.2-PLT-3-INF-SQUAD.INFCO-A.3-CABN.1-UA.ARMY.MIL-JP8.xml"
+        get_inventory  "FCS-ICV-0.3-PLT-WPN-SQUAD.INFCO-B.1-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ICV-0.3-PLT-WPN-SQUAD.INFCO-B.1-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ICV-0.3-PLT-WPN-SQUAD.INFCO-B.1-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-C2V-1.CO-HQ.INFCO-A.3-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-C2V-1.CO-HQ.INFCO-A.3-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-C2V-1.CO-HQ.INFCO-A.3-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-RS-V-2.2-PLT.RECON-DET.2-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-RS-V-2.2-PLT.RECON-DET.2-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-RS-V-2.2-PLT.RECON-DET.2-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-MCS-2.1-PLT.MCSCO-A.2-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-MCS-2.1-PLT.MCSCO-A.2-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-MCS-2.1-PLT.MCSCO-A.2-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-MCS-2.2-PLT.MCSCO-B.3-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-MCS-2.2-PLT.MCSCO-B.3-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-MCS-2.2-PLT.MCSCO-B.3-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "NLOS-Mortar-3.1-PLT.MORTAR-BTY.3-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:NLOS-Mortar-3.1-PLT.MORTAR-BTY.3-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-NLOS-Mortar-3.1-PLT.MORTAR-BTY.3-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "NLOS-Cannon-2.BTY-C-1-CANNON-PLT.NLOS-BN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:NLOS-Cannon-2.BTY-C-1-CANNON-PLT.NLOS-BN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-NLOS-Cannon-2.BTY-C-1-CANNON-PLT.NLOS-BN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-ARV-A-5.SUPPORT-SECTION.1-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ARV-A-5.SUPPORT-SECTION.1-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ARV-A-5.SUPPORT-SECTION.1-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-ARV-R-0.3-PLT.MCSCO-A.1-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-ARV-R-0.3-PLT.MCSCO-A.1-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-ARV-R-0.3-PLT.MCSCO-A.1-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-MV-4.MED-PLT-EVAC-SECTION.3-CABN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-MV-4.MED-PLT-EVAC-SECTION.3-CABN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-MV-4.MED-PLT-EVAC-SECTION.3-CABN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FCS-RMV-9.MAINT-PLT.FSB.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FCS-RMV-9.MAINT-PLT.FSB.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FCS-RMV-9.MAINT-PLT.FSB.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FTTS-U-C2-3.SIT-AWARE-TEAM.BIC.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FTTS-U-C2-3.SIT-AWARE-TEAM.BIC.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FTTS-U-C2-3.SIT-AWARE-TEAM.BIC.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FTTS-U-SPT-5.RADAR-SECTION.NLOS-BN.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FTTS-U-SPT-5.RADAR-SECTION.NLOS-BN.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FTTS-U-SPT-5.RADAR-SECTION.NLOS-BN.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FTTS-MS-36.DRY-CARGO-SECTION.FSB.1-UA.ARMY.MIL", "JP8:NSN/9130010315816:FTTS-MS-36.DRY-CARGO-SECTION.FSB.1-UA.ARMY.MIL", "INV/#{@modifier}/INV#{@modifier}-FTTS-MS-36.DRY-CARGO-SECTION.FSB.1-UA.ARMY.MIL-JP8.xml"
+
+        get_inventory  "FTTS-AMB-3.AREA-SPT-EVAC-SECTION.FSB.1-UA.ARMY.MIL
+", "JP8:NSN/9130010315816:", "INV/#{@modifier}/INV#{@modifier}--JP8.xml"
 
       end
       
       def get_inventory(agent, asset, file)
-        cougaar_agent = @run.society.agents[agent]
+	cougaar_agent = @run.society.agents[agent]
         if cougaar_agent
           list, uri = Cougaar::Communications::HTTP.get("#{cougaar_agent.uri}/list")
           if uri
@@ -68,6 +93,45 @@ module Cougaar
           end
         else
           @run.error_message "Inventory failed, unknown agent: #{agent}"
+        end
+      end
+    end
+  
+    # Sample the inventory of a society in predefined places
+    class SampleInventory < Cougaar::Action
+      PRIOR_STATES = ["SocietyRunning"]
+
+      attr_accessor :asset, :agent, :protocol, :society, :runport, :runhost
+
+      # Take the asset to get the inventory for at this agent
+      def initialize(run, agent, asset, file)
+        super(run)
+          @file = file
+          @asset = asset
+          @agent = agent
+          @protocol = "http" 
+      end
+
+      def save(result)
+        File.open(@file, "wb") do |file|
+          file.puts result
+        end
+        @run.archive_and_remove_file(@file, "Inventory for agent: #{@agent} asset: #{@asset}")
+      end
+
+      def perform
+        cougaar_agent = @run.society.agents[@agent]
+        if cougaar_agent
+          list, uri = Cougaar::Communications::HTTP.get("#{cougaar_agent.uri}/list")
+          if uri
+            #puts "SampleInventory: About to do put to: #{uri.scheme}://#{uri.host}:#{uri.port}/$#{@agent}/log_inventory for #{@asset}"
+            resp = Cougaar::Communications::HTTP.put("#{uri.scheme}://#{uri.host}:#{uri.port}/$#{@agent}/log_inventory", @asset)
+            save(resp)
+          else
+            @run.error_message "Inventory failed to redirect to agent: #{@agent}"
+          end
+        else
+          @run.error_message "Inventory failed, unknown agent: #{@agent}"
         end
       end
     end
