acb6969abafe050bce177201119e7007bd1840a3
mbarger <mbarger>
2004-04-06 19:39:49 +0000

    removed redundent action

--- acme/acme_scripting/src/lib/ultralog/ua-inventory.rb
+++ acme/acme_scripting/src/lib/ultralog/ua-inventory.rb
@@ -97,44 +97,6 @@ module Cougaar
       end
     end
   
-    # Sample the inventory of a society in predefined places
-    class SampleInventory < Cougaar::Action
-      PRIOR_STATES = ["SocietyRunning"]
-
-      attr_accessor :asset, :agent, :protocol, :society, :runport, :runhost
-
-      # Take the asset to get the inventory for at this agent
-      def initialize(run, agent, asset, file)
-        super(run)
-          @file = file
-          @asset = asset
-          @agent = agent
-          @protocol = "http" 
-      end
-
-      def save(result)
-        File.open(@file, "wb") do |file|
-          file.puts result
-        end
-        @run.archive_and_remove_file(@file, "Inventory for agent: #{@agent} asset: #{@asset}")
-      end
-
-      def perform
-        cougaar_agent = @run.society.agents[@agent]
-        if cougaar_agent
-          list, uri = Cougaar::Communications::HTTP.get("#{cougaar_agent.uri}/list")
-          if uri
-            #puts "SampleInventory: About to do put to: #{uri.scheme}://#{uri.host}:#{uri.port}/$#{@agent}/log_inventory for #{@asset}"
-            resp = Cougaar::Communications::HTTP.put("#{uri.scheme}://#{uri.host}:#{uri.port}/$#{@agent}/log_inventory", @asset)
-            save(resp)
-          else
-            @run.error_message "Inventory failed to redirect to agent: #{@agent}"
-          end
-        else
-          @run.error_message "Inventory failed, unknown agent: #{@agent}"
-        end
-      end
-    end
   end
 end
 
