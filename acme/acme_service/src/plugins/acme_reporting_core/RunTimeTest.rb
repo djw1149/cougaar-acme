e264b03c9b79bebf0ae1a45a64cd67b5d42d4ddd
jnilsson <jnilsson>
2004-04-12 18:21:45 +0000

    Added total time calculation.  Table cells are now colored based on a comparison to the mean.

--- acme/acme_service/src/plugins/acme_reporting_core/RunTimeTest.rb
+++ acme/acme_service/src/plugins/acme_reporting_core/RunTimeTest.rb
@@ -2,9 +2,10 @@ require "parsedate"
 
 module ACME
   module Plugins
-    RunTimeData = Struct.new("RunTimeData", :type, :interrupted, :load_time, :start_time, :stage_times)
+
     
     class RunTimeTest
+      RunTimeData = Struct.new("RunTimeData", :type, :interrupted, :load_time, :start_time, :total_time, :stage_times)
       def initialize(archive, plugin, ikko)
         @archive = archive
         @ikko = ikko
@@ -33,16 +34,11 @@ module ACME
         end
       end
       
-      def get_timestamp(line, pattern)
-        ts = nil
-        if pattern.match(line)
-          match_vars = $~[1..-1] #save $1, $2, ...
-          line =~ /\](.*)::/ #extract the date from the line
-          pd = ParseDate.parsedate($1)
-          time = Time.mktime(*pd)
-          ts = [time, match_vars]
-        end
-        return ts
+      def get_timestamp(line)
+        line =~ /\](.*)::/ #extract the date from the line
+        pd = ParseDate.parsedate($1)
+        time = Time.mktime(*pd)
+        return time
       end
       
       def next_target(target, type=nil)
@@ -71,36 +67,43 @@ module ACME
         all_data = []
         start = nil
         current = nil
+        total_start = nil        
+
         run_log.each do |line|
-          if (ts = get_timestamp(line, pattern_table[target])) then
+          ts = get_timestamp(line)
+          if (md = pattern_table[target].match(line)) then            
             if (target == :start_run) then
-              current = RunTimeData.new("", false, Time.at(0).gmtime, Time.at(0).gmtime, [])
+              current = RunTimeData.new("", false, Time.at(0).gmtime, Time.at(0).gmtime, Time.at(0).gmtime, [])
+              total_start = ts
             elsif (target == :load_time_start) then
-              start = ts[0]
-              current.type = ts[1][0]
+              start = ts
+              current.type = md[1]
             elsif (target == :load_time_end) then
-              current.load_time = ts[0] - start
+              current.load_time = ts - start
             elsif (target == :start_time_start) then
-              start = ts[0]
+              start = ts
             elsif (target == :start_time_persistance_end || target == :start_time_scratch_end) then
-              current.start_time = ts[0] - start
+              current.start_time = ts - start
             elsif (target == :stage_time_start)
-              start = ts[0]
+              start = ts
             elsif (target == :stage_time_end) then
-              current.stage_times << ts[0] - start
+              current.stage_times << ts - start
             end
             target = next_target(target, current.type)
-          elsif (ts = get_timestamp(line, /INTERRUPT/)) then 
+          elsif (line =~ /INTERRUPT/) then 
             current.interrupted = true
             if (target == :stage_time_end)
-              current.stage_times << ts[0] - start
+              current.stage_times << ts - start
             end
             all_data << current
+            current = nil
             target = :start_run
-          elsif (target != :start_run && ts = get_timestamp(line, /Run:.*finished/)) then
+          elsif (target != :start_run && line =~ /Run:.*finished/) then
             all_data << current
+            current = nil
             target = :start_run
           end
+          current.total_time = ts - total_start if (!current.nil?)
         end
         all_data << current unless target == :start_run
         return all_data
@@ -142,32 +145,71 @@ module ACME
         all_data.each do |elem|
           if elem.class.to_s  == "Struct::RunTimeData" then
             data << elem[field] unless (elem[field].nil? || elem.type =~ /INTERRUPTED/)
-          else
+          else 
             data << elem[field] unless elem[field].nil?
           end
         end
         return data
       end
-      
+
+      def collect_stat(all_data)
+        load_times = collect_elements(all_data, "load_time")
+        start_times = collect_elements(all_data, "start_time")
+        total_times = collect_elements(all_data, "total_time")
+        all_stage_times = collect_elements(all_data, "stage_times")
+        stage_times = []
+        all_stage_times[0].each_index do |i|
+          stage_times << collect_elements(all_stage_times, i)
+        end
+        
+        stat_data = RunTimeData.new
+        stat_data.load_time = yield load_times
+        stat_data.start_time = yield start_times
+        stat_data.total_time = yield total_times
+        stat_data.stage_times = []
+        stage_times.each do |time|
+          stat_data.stage_times << (yield time)
+        end
+        return stat_data
+      end              
+
       def format_time(n)
         return Time.at(n).gmtime.strftime("%H:%M:%S")
       end
-      
-      def get_format_string(data, i)
-        str = sprintf("%-7s", (data.interrupted ? "#{i+1}INT" : (i+1).to_s))
-        str << sprintf("%-12s", data.load_time.strftime("%H:%M:%S"))
-        str << sprintf("%-12s", data.start_time.strftime("%H:%M:%S"))
-        data.stage_times.each do |stage_time|
-          str << sprintf("%-12s", stage_time.strftime("%H:%M:%S"))
-        end
-        return str
+
+      def get_class(val, mean, stddev)
+        return val <= (mean + 2 *stddev) ? "#00DD00" : "#FF0000"
       end
-            
+
+      def row_output(row, data, mean, stddev)
+        row_string = @ikko["cell_template.html", {"data"=>row}]
+        cellclass = get_class(data.total_time, mean.total_time, stddev.total_time)
+        row_string <<  @ikko["cell_template.html", {"data"=>format_time(data.total_time), "options"=>"bgcolor=#{cellclass}"}]
+
+        cellclass = get_class(data.load_time, mean.load_time, stddev.load_time)
+        row_string <<  @ikko["cell_template.html", {"data"=>format_time(data.load_time), "options"=>"bgcolor=#{cellclass}"}]
+
+        cellclass = get_class(data.start_time, mean.start_time, stddev.start_time)
+        row_string <<  @ikko["cell_template.html", {"data"=>format_time(data.start_time), "options"=>"bgcolor=#{cellclass}"}]
+
+        data.stage_times.each_index do |i|
+          cellclass = get_class(data.stage_times[i], mean.stage_times[i], stddev.stage_times[i])
+          row_string <<  @ikko["cell_template.html", {"data"=>format_time(data.stage_times[i]), "options"=>"bgcolor=#{cellclass}"}]
+        end
+        return row_string
+      end
+
+
       def html_output(all_data)
         ikko_data = {}
         ikko_data["description_link"]="run_times_description.html"
         ikko_data["id"] = @archive.base_name
-        headers = ["Run", "Load Time", "Start Time", "Stage Time[s]"]
+
+        mean_data = collect_stat(all_data){|x| mean(x)}
+        stddev_data = collect_stat(all_data){|x| standard_deviation(x)}
+        percent_data = collect_stat(all_data){|x| std_percent(x)}
+
+        headers = ["Run", "Total Time", "Load Time", "Start Time", "Stage Time[s]"]
         header_string = ""
         headers.each do |h|
           header_string << @ikko["header_template.html", {"data"=>h}]
@@ -175,45 +217,35 @@ module ACME
         table_string = @ikko["row_template.html", {"data"=>header_string}]
         
         all_data.each_index do |i|
-          row_string = @ikko["cell_template.html", {"data"=>(i+1)}]
-          row_string <<  @ikko["cell_template.html", {"data"=>format_time(all_data[i].load_time)}]
-          row_string <<  @ikko["cell_template.html", {"data"=>format_time(all_data[i].start_time)}]
-          all_data[i].stage_times.each do |stage_time|
-            row_string <<  @ikko["cell_template.html", {"data"=>format_time(stage_time)}]
-          end
+          row_string = row_output((i+1).to_s, all_data[i], mean_data, stddev_data)
           table_string << @ikko["row_template.html", {"data"=>row_string}]
         end
 
-        load_times = collect_elements(all_data, "load_time")
-        start_times = collect_elements(all_data, "start_time")
-        all_stage_times = collect_elements(all_data, "stage_times")
-        stage_times = []
-        all_stage_times[0].each_index do |i|
-          stage_times << collect_elements(all_stage_times, i)
-        end
-        
         row_string = @ikko["cell_template.html", {"data"=>"MEAN"}]
-        row_string << @ikko["cell_template.html", {"data"=>format_time(mean(load_times))}]
-        row_string << @ikko["cell_template.html", {"data"=>format_time(mean(start_times))}]
-        stage_times.each do |stage|
-          row_string << @ikko["cell_template.html", {"data"=>format_time(mean(stage))}]
+        row_string << @ikko["cell_template.html", {"data"=>format_time(mean_data.total_time)}]
+        row_string << @ikko["cell_template.html", {"data"=>format_time(mean_data.load_time)}]
+        row_string << @ikko["cell_template.html", {"data"=>format_time(mean_data.start_time)}]
+
+        mean_data.stage_times.each do |stage|
+          row_string << @ikko["cell_template.html", {"data"=>format_time(stage)}]
         end
         table_string << @ikko["row_template.html", {"data"=>row_string}]
 
-
         row_string = @ikko["cell_template.html", {"data"=>"STDDEV"}]
-        row_string << @ikko["cell_template.html", {"data"=>format_time(standard_deviation(load_times))}]
-        row_string << @ikko["cell_template.html", {"data"=>format_time(standard_deviation(start_times))}]
-        stage_times.each do |stage|
-          row_string << @ikko["cell_template.html", {"data"=>format_time(standard_deviation(stage))}]
+        row_string << @ikko["cell_template.html", {"data"=>format_time(stddev_data.total_time)}]
+        row_string << @ikko["cell_template.html", {"data"=>format_time(stddev_data.load_time)}]
+        row_string << @ikko["cell_template.html", {"data"=>format_time(stddev_data.start_time)}]
+        stddev_data.stage_times.each do |stage|
+          row_string << @ikko["cell_template.html", {"data"=>format_time(stage)}]
         end
         table_string << @ikko["row_template.html", {"data"=>row_string}]
 
         row_string = @ikko["cell_template.html", {"data"=>"STDDEV Percent"}]
-        row_string << @ikko["cell_template.html", {"data"=>std_percent(load_times)}]
-        row_string << @ikko["cell_template.html", {"data"=>std_percent(start_times)}]
-        stage_times.each do |stage|
-          row_string << @ikko["cell_template.html", {"data"=>std_percent(stage)}]
+        row_string << @ikko["cell_template.html", {"data"=>percent_data.total_time}]
+        row_string << @ikko["cell_template.html", {"data"=>percent_data.load_time}]
+        row_string << @ikko["cell_template.html", {"data"=>percent_data.start_time}]
+        percent_data.stage_times.each do |stage|
+          row_string << @ikko["cell_template.html", {"data"=>stage}]
         end
         table_string << @ikko["row_template.html", {"data"=>row_string}]
 
@@ -221,48 +253,16 @@ module ACME
         return @ikko["time_report.html", ikko_data]
       end
 
-      def chart_output(all_data)
-        str = ""
-        str << sprintf("%-7s%-12s%-12s%-12s", "Run", "Load Time", "Start Time", "Stage Time[s]\n")
-        all_data.each_index do |i|
-          str << get_format_string(all_data[i], i)
-          str << "\n"
-        end
-      
-        load_times = collect_elements(all_data, "load_time")
-        start_times = collect_elements(all_data, "start_time")
-        all_stage_times = collect_elements(all_data, "stage_times")
-        stage_times = []
-        all_stage_times[0].each_index do |i|
-          stage_times << collect_elements(all_stage_times, i)
-        end
-        
-        str << sprintf("%-7s%-12s%-12s", "MEAN", format_time(mean(load_times)), format_time(mean(start_times)))
-        stage_times.each do |stage|
-          str << sprintf("%-12s", format_time(mean(stage)))
-        end
-        str << "\n"
-
-        str << sprintf("%-7s%-12s%-12s", "STDDEV", format_time(standard_deviation(load_times)), format_time(standard_deviation(start_times)))
-        stage_times.each do |stage|
-          str << sprintf("%-12s", format_time(standard_deviation(stage)))
-        end
-        str << "\n"
-        
-        str << sprintf("%-7s%-12s%-12s", "VAR", format_time(variance(load_times)), format_time(variance(start_times)))
-        stage_times.each do |stage|
-          str << sprintf("%-12s", format_time(variance(stage)))
-        end
-        str << "\n"
-        return str
-      end
-  
       def create_description
         ikko_data = {}
         ikko_data["name"]="Run Time Test"
         ikko_data["title"] = "Run Time Test Description"
         ikko_data["description"] = "Creates a table showing the time the society took for loading, starting, and each stage"
-        ikko_data["description"] << " for each run in the run.log."
+        ikko_data["description"] << " for each run in the run.log.  Green boxes indicate times that are less than the mean"
+        ikko_data["description"] << " plus two standard deviations.  Red boxes indicate times that are greater than the mean"
+        ikko_data["description"] << " plus two standard deviations.  The colors of the boxes do not currently determine success"
+        ikko_data["description"] << " or failure of the test."
+ 
         success_table = {"success"=>"Run.log present",
                          "partial"=>"not used",
                          "fail"=>"run.log not found"}
