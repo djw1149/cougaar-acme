2ba0c8259c357490658f20d8e4baff81df7354f7
jnilsson <jnilsson>
2004-04-15 17:56:20 +0000

    New table output format.

--- acme/acme_service/src/plugins/acme_reporting_core/CompletionTest.rb
+++ acme/acme_service/src/plugins/acme_reporting_core/CompletionTest.rb
@@ -1,12 +1,49 @@
 module ACME
   module Plugins
 
+    SUCCESS = 0
+    PARTIAL = 1
+    FAIL = 2
+
+    class AgentCompletionData
+      attr_accessor :error
+      attr_reader :name
+        
+      def initialize (name)
+        @name = name
+        @comp_data = {}
+        @error = SUCCESS
+      end
+        
+      def []=(tag, value)
+        @comp_data[tag]=value
+      end
+        
+      def [](tag)
+        return @comp_data[tag]
+      end
+        
+      def <=>(rhs)
+        return rhs.error <=> @error if (@error != rhs.error)
+        return @name <=> rhs.name
+      end
+        
+      def get_fields
+        fields = []
+        fields << "NumTasks"
+        fields << "Ratio"
+        @comp_data.keys.sort.each do |field|
+          fields << field unless fields.include?(field)
+        end
+        return fields
+      end
+    end
+        
+        
+        
     class CompletionTest
-      AgentData = Struct.new("AgentData", :name, :comp_data, :error)
+      
       FileData = Struct.new("FileData", :agents, :totals)
-      SUCCESS = 0
-      PARTIAL = 1
-      FAIL = 2
       TOLERENCE = 0.10
 
       def initialize(archive, plugin, ikko)
@@ -29,7 +66,8 @@ module ACME
             benchmark_data = nil
             benchmark_data = get_file_data(File.new(benchmark_file)) unless benchmark_file.nil?
             result = analyze(data, benchmark_data)
-            
+            data.agents.sort! #analyze function may change ordering if agents fail
+        
             if result == SUCCESS then
               report.success
             elsif result == PARTIAL then
@@ -77,15 +115,15 @@ module ACME
             line.chomp!
             agent_name = line.split(/=/)[1]
             agent_name.delete!("\'>")
-            curr_agent = AgentData.new(agent_name, {}, SUCCESS)
+            curr_agent = AgentCompletionData.new(agent_name)
           elsif (line =~ /<(.+)>(.+)<\/\1>/) then
             if !curr_agent.nil? then
               if ($1 == "Ratio") then
-                curr_agent.comp_data[$1] = $2.to_f
+                curr_agent[$1] = $2.to_f
               elsif ($1 == "TimeMillis") then
-                curr_agent.comp_data[$1] = Time.at($2.to_i/1000)
+                curr_agent["Time"] = Time.at($2.to_i/1000).strftime("%b/%d/%Y-%H:%M:%S")
               else
-                curr_agent.comp_data[$1] = $2.to_i
+                curr_agent[$1] = $2.to_i
               end
             else
               data.totals[$1] = $2.to_i
@@ -95,7 +133,7 @@ module ACME
             curr_agent = nil
           end
         end
-        data.agents.sort!{|x, y| x.name <=> y.name}
+        data.agents.sort!
         return data
       end
 
@@ -107,17 +145,16 @@ module ACME
           e = task_test(data, benchmark)
           error = (error > e ? error : e)
         end
-        
         return error
       end
      
       def ratio_test(data)
         error = SUCCESS
         data.agents.each do |agent|
-          if (agent.comp_data["Ratio"] < 1.0 && agent.comp_data["Ratio"] >= 0.90) then
+          if (agent["Ratio"] < 1.0 && agent["Ratio"] >= 0.90) then
             error = PARTIAL if error == SUCCESS
             agent.error = PARTIAL if agent.error == SUCCESS
-          elsif (agent.comp_data["Ratio"] < 0.90) then
+          elsif (agent["Ratio"] < 0.90) then
             error = FAIL
             agent.error = FAIL
           end
@@ -130,25 +167,14 @@ module ACME
         data.agents.each do |agent|
           benchmark_agent = (benchmark.agents.collect{|x| agent.name == x.name ? x : nil}.compact)[0]
           next if benchmark_agent.nil?
-          low_bound = (benchmark_agent.comp_data["NumTasks"] * (1 - TOLERENCE)).to_i
-          if agent.comp_data["NumTasks"] < low_bound then
+          low_bound = (benchmark_agent["NumTasks"] * (1 - TOLERENCE)).to_i
+          if agent["NumTasks"] < low_bound then
             error = FAIL
             agent.error = FAIL
           end
         end
         return error
       end
-
-      def get_fields(keys)
-        fields = []
-        fields << "NumTasks"
-        fields << "Ratio"
-        keys.sort.each do |field|
-          fields << field unless fields.include?(field)
-        end
-        return fields
-      end
-
 
       def html_output(data, stage)
         ikko_data = {}
@@ -159,7 +185,7 @@ module ACME
           ikko_data["totals"] << "#{key}:  #{data.totals[key]}"        
         end
         headers = ["Agent Name"]
-        fields = get_fields(data.agents[0].comp_data.keys)
+        fields = data.agents[0].get_fields
         headers << fields
         headers.flatten!
         header_row = ""
@@ -171,7 +197,7 @@ module ACME
         data.agents.each do |agent|
           agent_row = @ikko["cell_template.html", {"data"=>agent.name,"options"=>""}]
           fields.each do |key|
-            agent_row << @ikko["cell_template.html", {"data"=>agent.comp_data[key],"options"=>""}]
+            agent_row << @ikko["cell_template.html", {"data"=>agent[key],"options"=>""}]
           end
           options = ""
           if (agent.error == SUCCESS) then
@@ -192,16 +218,16 @@ module ACME
         ikko_data["name"]="Completion Report"
         ikko_data["title"] = "Completion Report Description"
         ikko_data["description"] = "Creates a table from the information in completion report xml files.  Currently the test"
-        ikko_data["description"] << " is based on the tasks and ratio fields.  A node is green if it has a ratio of 1.0 and "
-        ikko_data["description"] << "is within 10% of the baseline number of tasks.  A node is yellow is it has a ratio of"
-        ikko_data["description"] << " at least 0.95 and is within 10% of the baseline tasks.  A node is red otherwise."
+        ikko_data["description"] << " is based on the tasks and ratio fields.  An agent is green if it has a ratio of 1.0 and "
+        ikko_data["description"] << "is within 10% of the baseline number of tasks.  An agent is yellow is it has a ratio of"
+        ikko_data["description"] << " at least 0.95 and is within 10% of the baseline tasks.  An agent is red otherwise."
         ikko_data["description"] << " If the appropriate benchmark file can not be found because the subject file is named"
-        ikko_data["description"] << "in a nonstandard way, then the number of tasks test will not be run.  In this case"
+        ikko_data["description"] << " in a nonstandard way, then the number of tasks test will not be run.  In this case"
         ikko_data["description"] << " the report will be at most PARTIAL SUCCESS."
 
-        success_table = {"success"=>"Every node has a ratio of 1.0 and is within 10% of the baseline",
-                         "partial"=>"Baseline missing or at least one node is yellow as described above and none are red",
-                         "fail"=>"At least one node has a ratio less than 0.90 or is not within 10% of the baseline"}
+        success_table = {"success"=>"Every agent has a ratio of 1.0 and is within 10% of the baseline",
+                         "partial"=>"Baseline missing or at least one agent is yellow as described above and none are red",
+                         "fail"=>"At least one agent has a ratio less than 0.90 or is not within 10% of the baseline"}
         ikko_data["table"] = @ikko["success_template.html", success_table]
         return @ikko["description.html", ikko_data]
       end
