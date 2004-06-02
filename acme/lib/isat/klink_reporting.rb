4fbbe2e7b7695936f1b2b468523ac97e41321d63
dpeugh <dpeugh>
2004-06-02 19:43:24 +0000

    Fixed filenames.

--- acme/lib/isat/klink_reporting.rb
+++ acme/lib/isat/klink_reporting.rb
@@ -9,7 +9,14 @@ description: Report on the K-Links at good points in the run.
 ["during_stage_1", "during_stage_2", "during_stage_3", "during_stage_4", "during_stages_3_4", "during_stage_5", "during_stage_6", "during_stages_5_6", "during_stage_7", "after_stage_1", "after_stage_2", "after_stage_3", "after_stage_4", "after_stages_3_4", "after_stage_5", "after_stage_6", "after_stage_5_6", "after_stage_7", "starting_stage", "ending_stage" ].each do |tag|
   unless (sequence.index_of(tag).nil?) then
     insert_after tag do
-      do_action "RouterInformation", "router-#{tag}.xml"
+      case tag
+        when "starting_stage"
+          do_action "RouterInformation", "router-during_stage_restore.xml"
+        when "ending_stage"
+          do_action "RouterInformation", "router-after_stage_restore.xml"
+        else
+          do_action "RouterInformation", "router-#{tag}.xml"
+      end
     end
   end
 end
