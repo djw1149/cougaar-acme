b145e655418bf309fb9971bfcf82c9737c2a13a1
rich <rich>
2004-06-29 17:41:43 +0000

    thread the http agent move request in keep society sync'd

--- acme/acme_scripting/src/lib/cougaar/society_control.rb
+++ acme/acme_scripting/src/lib/cougaar/society_control.rb
@@ -371,7 +371,9 @@ module Cougaar
   #                result = Cougaar::Communications::HTTP.get("#{node.uri}/agentQuiescenceState?dead=#{agent_name}", 60)
 
                   # Remove it to free up resources - See ubug 13550
-                  result = Cougaar::Communications::HTTP.get("#{node.uri}/move?op=Remove&mobileAgent=#{agent_name}&destNode=#{node.name}&action=Add", 60)
+                  Thread.new(node.uri, node.name, agent_name) do |uri, node_name, agent| 
+                    result = Cougaar::Communications::HTTP.get("#{uri}/move?op=Remove&mobileAgent=#{agent}&destNode=#{node_name}&action=Add", 60)
+                  end
 
                   # FIXME: look at the result to see if this actually did something
                   # If it did, log that fact
