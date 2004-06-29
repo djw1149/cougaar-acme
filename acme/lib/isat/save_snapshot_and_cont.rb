e2bcfabcd6f2f12b4029c317cae43420b66636e3
mbarger <mbarger>
2004-06-29 14:48:58 +0000

    remove the freeze and thaw.  A recent test showed that the society didn't function well after a thaw.

--- acme/lib/isat/save_snapshot_and_cont.rb
+++ acme/lib/isat/save_snapshot_and_cont.rb
@@ -6,10 +6,10 @@ description: Saves a persistence snapshot but keeps running
 =end
 
 insert_after parameters[:snapshot_location] do
-  do_action "FreezeSociety"
+#  do_action "FreezeSociety"
   do_action "Sleep", 10.minutes
   do_action "SynchronizeSocietyTime"
   do_action "SavePersistenceSnapshot", parameters[:snapshot_name]
-  do_action "ThawSociety"
-  do_action "Sleep", 1.minutes
+#  do_action "ThawSociety"
+#  do_action "Sleep", 1.minutes
 end
