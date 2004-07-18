4122d56ec6c6832ab50f97cd4e46993281d60285
rich <rich>
2004-07-18 19:33:37 +0000

    updated license

--- acme/acme_service/bin/install.rb
+++ acme/acme_service/bin/install.rb
@@ -1,5 +1,31 @@
 #! /usr/bin/env ruby
 
+=begin
+ * <copyright>  
+ *  Copyright 2001-2004 InfoEther LLC  
+ *  Copyright 2001-2004 BBN Technologies
+ *
+ *  under sponsorship of the Defense Advanced Research Projects  
+ *  Agency (DARPA).  
+ *   
+ *  You can redistribute this software and/or modify it under the 
+ *  terms of the Cougaar Open Source License as published on the 
+ *  Cougaar Open Source Website (www.cougaar.org <www.cougaar.org> ).   
+ *   
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * </copyright>  
+=end
+
 require 'ftools'
 
 SRC = 'src'
@@ -66,7 +92,7 @@ opts.each do |opt, arg|
   when '--target'
     @destdir = arg
   when '--help'
-    puts "Installs the ACME Service.\nUsage:\n\t#$0 -j <jabberhost> [ [-n] [-h] [-t <dir>] [-a <account>] [-p <pwd>]"
+    puts "Installs the ACME Service.\nUsage:\n\t#$0 -r <routerhost> [ [-n] [-h] [-t <dir>] [-a <account>] [-p <pwd>]"
     puts "\t\t\t[-v <jvm path>] [-c <cougaar install path>] [-l <server props path>]"
     puts "\t\t\t[-b <command prefix>] [-e <command suffix>] [-w <user>] -u]"
     puts "\t-j --jabber-host\tThe jabber host (message router or this required)."
