309438de644d3bcff7fdb71482a3244ca8f4f59b
rich <rich>
2004-07-19 22:17:28 +0000

    added ping example society

---
+++ acme/acme_scripting/examples/ping/ping-actions.rb
@@ -0,0 +1,109 @@
+##
+#  <copyright>
+#  Copyright 2002 InfoEther, LLC
+#  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the Cougaar Open Source License as published by
+#  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+#
+#  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+#  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+#  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+#  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+#  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+#  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+#  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#  PERFORMANCE OF THE COUGAAR SOFTWARE.
+# </copyright>
+#
+
+require 'cougaar/communities'
+
+#
+# Utility methods for creating a ping society
+#
+
+module Cougaar
+  module Actions
+    class AddPing < Cougaar::Action
+      PRIOR_STATES = ["SocietyLoaded"]
+      DOCUMENTATION = Cougaar.document {
+        @description = "Transform society with added ping components"
+        @parameters = [
+          {:source=> "required, Source Agent"},
+          {:dest=> "required, Destination Agent"},
+          {:args=> "optional, hash of arguments"}
+        ]
+        @example = "do_action 'AddPing', 'AgentA', 'AgentB', {'eventMillis' => '10000'}"
+      }
+      def initialize(run, source, dest, args)
+        super(run)
+        @source = source
+        @dest = dest
+        @args = args
+      end
+      def perform
+        srcAgent = @run.society.agents[@source]
+        if !srcAgent
+          raise "Agent #{@source} does not exist in society"
+        end
+        destAgent = @run.society.agents[@dest]
+        if !destAgent
+          raise "Agent #{@dest} does not exist in society"
+        end
+
+        srcAgent.add_component("org.cougaar.core.mobility.ping.PingAdderPlugin") do |c|
+          c.classname = "org.cougaar.core.mobility.ping.PingAdderPlugin"
+          c.add_argument("target=#{@dest}")
+          @args.each_pair {|key, value|
+            c.add_argument("#{key}=#{value}")
+          }
+        end
+      end
+    end
+    class SetupPingTimers < Cougaar::Action
+      PRIOR_STATES = ["SocietyLoaded"]
+      DOCUMENTATION = Cougaar.document {
+        @description = "Transform society with ping components"
+        @parameters = [
+          {:wake_time=> "required, Time between ping checks"}
+        ]
+        @example = "do_action 'SetupPingTimers', '1000'"
+      }
+      def initialize(run, wake_time)
+        super(run)
+        @wake_time = wake_time
+      end
+      def perform
+        @run.society.each_agent do |agent|
+          if agent.has_component?("org.cougaar.core.mobility.ping.PingAdderPlugin")
+            unless agent.has_component?("org.cougaar.core.mobility.ping.PingTimerPlugin")
+              c = agent.add_component("org.cougaar.core.mobility.ping.PingTimerPlugin")
+              c.classname = "org.cougaar.core.mobility.ping.PingTimerPlugin"
+              c.add_argument("#{@wake_time}")
+            end
+          end
+        end
+      end
+    end
+    class SetupCommunityPlugins < Cougaar::Action
+      PRIOR_STATES = ["SocietyLoaded"]
+      DOCUMENTATION = Cougaar.document {
+        @description = "Transform society with community plugins"
+        @parameters = []
+        @example = "do_action 'SetupCommunityPlugins'"
+      }
+      def perform
+        @run.society.each_agent(false) do |agent|
+          unless agent.has_component?('org.cougaar.community.CommunityPlugin')
+            agent.add_component('org.cougaar.community.CommunityPlugin') do |c|
+              c.classname = 'org.cougaar.community.CommunityPlugin'
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ acme/acme_scripting/examples/ping/run-ping.rb
@@ -0,0 +1,82 @@
+SOCIETY_FILE =  'ping-society.xml'
+HOST_FILE =     'ping-hosts.xml'
+LAYOUT_FILE =   'ping-layout.xml'
+
+begin
+  require 'cougaar/scripting'
+rescue LoadError => e
+  $:.unshift File.join('..', '..', 'src', 'lib')
+  $:.unshift File.join('..', '..', '..', 'acme_service', 'src', 'redist')
+  require 'cougaar/scripting'
+end
+
+require './ping-actions.rb'
+
+require 'socket'
+require 'rexml/document'
+
+Cougaar::ExperimentMonitor.enable_stdout
+Cougaar::ExperimentMonitor.enable_logging
+
+Cougaar.new_experiment("MiniPing-Test").run(1) {
+
+  # read the basic society definition
+  #
+  # also see the "create_society.rb" class, which illustrates how
+  # to create a society from scratch within ruby.
+  # useage: do_action "BuildSocietyFromScratch", "MiniPing"
+  do_action "LoadSocietyFromXML", SOCIETY_FILE
+
+  # transform the basic society to use our host-node layout
+  do_action "LayoutSociety", LAYOUT_FILE, HOST_FILE
+
+  # add the community plugins
+  do_action "SetupCommunityPlugins"
+
+  # add a ping from AgentA to AgentB, and have it generate event
+  # statistics once every 10 seconds
+  # 
+  # see the org.cougaar.core.mobility.ping.PingAdderPlugin for
+  # additional options.
+  do_action "AddPing", "AgentA", "AgentB", {'eventMillis' => '10000'}
+
+  # add the ping manager plugins
+  #
+  # A ping manager is required for every agent that contains a
+  # ping adder plugin.  This rule searches for the agents and
+  # adds the manager plugins.
+  #
+  # The "1000" is the time between ping timeout and event checks.
+  # One second is fine for most tests.
+  do_action "SetupPingTimers", 1000
+
+  # load local rules (ping_env.rule)
+  do_action "TransformSociety", false, "."
+
+  # optional: save the society to an XML file for easy debugging
+  do_action "SaveCurrentSociety", "savedSociety.xml"
+
+  # start jabber
+  #
+  # replace the last parameter with your jabber server's host name 
+  do_action "StartCommunications"
+
+  do_action "VerifyHosts"
+
+  # optional: print the cougaar events
+  #
+  # this will also print the ping statistics events
+  do_action "GenericAction" do |run| 
+     run.comms.on_cougaar_event do |event| 
+       puts event 
+     end 
+  end 
+
+  do_action "StartSociety"
+
+  # however long you want to run
+  do_action "Sleep", 5.minutes
+
+  do_action "StopSociety"
+  do_action "StopCommunications"
+}
