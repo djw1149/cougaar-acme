011fb0ba3fc8c9cd9eeee9c7b10fd59779db9ca0
mbarger <mbarger>
2004-08-20 18:32:42 +0000

    specify node name rather than host name for migration.  This makes it society independent

--- acme/lib/isat/migrate.rb
+++ acme/lib/isat/migrate.rb
@@ -3,11 +3,11 @@
 include_path: migrate.rb
 description: Uinit Reafiliation Migration
              migrate_location - location in the script to do this
-             host_name - host to migrate
+             node_name - node on the host to migrate
              target_network - network name of destination
 
 =end
 
 insert_after parameters[:migrate_location] do
-  do_action "Migrate", parameters[:host_name],parameters[:target_network]
+  do_action "Migrate", parameters[:node_name],parameters[:target_network]
 end
