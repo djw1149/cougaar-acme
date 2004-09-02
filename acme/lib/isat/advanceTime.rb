9c31d0d4c7b958b9f21fe9f060f56956d786a4c0
mbarger <mbarger>
2004-09-02 20:09:12 +0000

    fix some errors

--- acme/lib/isat/advanceTime.rb
+++ acme/lib/isat/advanceTime.rb
@@ -7,7 +7,7 @@ description: does the advance time between stages. Give it the time to advance, 
 
 insert_after parameters[:advance_location] do
   do_action "InfoMessage", "Advancing time #{parameters[:days]}, 1 day steps, quiescing between steps"
-  do_action "AdvanceTime", eval(parameters[:days].to_s)
-  include "post_stage_data.inc", "PostAdvanceAt#{parameters[:advance_location]}"
+  do_action "AdvanceTime", parameters[:days].days
+#  include "post_stage_data.inc", "PostAdvanceAt#{parameters[:advance_location]}"
 end
 
