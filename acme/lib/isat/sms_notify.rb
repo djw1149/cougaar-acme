dbd0c89781949acc1d7797a9cf18649d53f1464b
mbarger <mbarger>
2004-09-08 15:16:48 +0000

    use new SMS code in ultralog/operator_utils.rb

--- acme/lib/isat/sms_notify.rb
+++ acme/lib/isat/sms_notify.rb
@@ -8,27 +8,8 @@ description: adds an on_archive hook which notifies art and mark of failed runs
 on_archive do |run, archive_fiile|
   log = File.read('run.log')
   
-  # numbers
-  art = '7039196618@vtext.com'
-  mark = '5716431333@vtext.com'
-  rich = '5713326896@voicestream.net'
-  joe = '7038696710@messaging.sprintpcs.com'
-  phonelist = [art, mark, rich, joe]
-
   #error checking
-  error = nil
   md = /ERROR: Could not verify Society: Could not access host: (.*)/.match(log)
-  error = "Verify failed on host #{md[1]}" if md
-
-  #send messages
-  if error
-    phonelist.each do |number|
-      IO.popen("mail -s '#{error}' #{number}", "w") do |io|
-        io.putc(4)
-        io.putc(13)
-      end
-      sleep 1
-    end
-  end   
+  Ultralog::OperatorUtils::SMSNotify.new.notify("Verify failed on host #{md[1]}") if md
 
 end  
