3c0aa5b74b5f2edc41ec83edc05ae591a183bbba
jnilsson <jnilsson>
2004-10-05 18:15:27 +0000

    Added xmx and free information.

--- acme/acme_service/src/plugins/acme_reporting_core/MemoryReport.rb
+++ acme/acme_service/src/plugins/acme_reporting_core/MemoryReport.rb
@@ -5,6 +5,7 @@ module ACME
 
       def initialize
         @data = {}
+        @xmx = {}
       end
 
       def []=(stage, node, value)
@@ -36,6 +37,14 @@ module ACME
       def nodes
         return @data[stages.last].keys
       end
+
+      def xmx(node)
+        return @xmx[node]
+      end
+ 
+      def set_xmx(node, val)
+        @xmx[node] = val
+      end
     end
 
 
@@ -52,12 +61,13 @@ module ACME
 
       def perform
         memory_files = @archive.files_with_description(/Memory usage file/)
+        node_files = @archive.files_with_description(/XML node config files/)
         if (!memory_files.empty?) then
           @archive.add_report("Mem", @plugin.plugin_configuration.name) do |report|
             all_data = []
             #get run data from the cache manager for the current run
             all_data <<  @cm.load(@archive.base_name, ArchiveMemoryData) do |name|
-              compile_memory_data(memory_files)
+              compile_memory_data(memory_files, node_files)
             end
 
             avg_files = [@archive.base_name]
@@ -125,17 +135,23 @@ module ACME
         return data
       end
 
-      def compile_memory_data(memory_files)
+      def compile_memory_data(memory_files, node_files)
         archive_data = ArchiveMemoryData.new
         memory_files.each do |file|
-	  data = read_file(file.name)
+	  data = read_memory_file(file.name)
           archive_data[data.stage, data.node_name] = data.memory
+          node_pattern = Regexp.new("#{data.node_name}.xml")
+          node_files.each do |node_file|
+            if node_pattern.match(node_file.name)
+              archive_data.set_xmx(data.node_name, get_xmx(node_file.name))
+              break
+            end
+          end
         end
         return archive_data
       end
 
-      
-      def read_file(filename)
+      def read_memory_file(filename)
         data = MemoryStructure.new
         stage = nil
         if filename =~ /memdata_(Stage.*?)\// then          
@@ -144,7 +160,7 @@ module ACME
           stage = $1
         end
         file = IO.readlines(filename)
-        data.node_name = file[0]
+        data.node_name = file[0].strip!
         memory = file[4].gsub(/VmRSS:/, "").strip 
         memory =~ /([0-9]+)/
         memory = $1.to_i
@@ -153,6 +169,16 @@ module ACME
         data.stage = stage
         return data
       end
+
+     def get_xmx(node_file)
+       IO.readlines(node_file).each do |line|
+         if (line =~ /-Xmx([0-9]+)m/)
+           return $1.to_i
+         end
+       end
+       return nil
+     end
+
 
       def average(all_data, stage, node)
         values = []
@@ -173,11 +199,10 @@ module ACME
         ikko_data["id"] = @archive.base_name
         ikko_data["description_link"] = "memory_description.html"
         ikko_data["files_link"] = "avg_list.html"
-        headers = ["Node"]
-        headers << run_data.stages
-        headers.flatten!
-                
-        row_string = ""
+        headers = run_data.stages
+        headers << "Xmx"
+              
+        row_string = @ikko["header_template.html", {"data"=>"Node", "options"=>"COLSPAN=2"}]
         headers.each do |header|
           row_string << @ikko["header_template.html", {"data"=>header}]
         end
@@ -185,25 +210,65 @@ module ACME
        
         
         run_data.sorted_nodes.each do |node|
-          row_string = @ikko["cell_template.html", {"data"=>node, "options"=>"ROWSPAN=2"}]
-          run_data.stages.each do |stage|
-            if (run_data[stage, node].nil?) then
-              row_string << @ikko["cell_template.html", {"data"=>"NODE NOT PRESENT", "options"=>"BGCOLOR=#ff0000"}]
-            else
-              row_string << @ikko["cell_template.html", {"data"=>"#{run_data[stage, node]} MB", "options"=>"BGCOLOR=#DDDDDD"}]
-            end
-          end
-          table_string << @ikko["row_template.html", {"data"=>row_string}]
-          
-          row_string = ""
-          run_data.stages.each do |stage|
-            row_string << @ikko["cell_template.html", {"data"=>"#{average(all_data, stage, node)} MB", "options"=>"BGCOLOR=#BBBBBB"}]
-          end
-          table_string << @ikko["row_template.html", {"data"=>row_string}]
+          table_string << node_row_string(all_data, run_data, node)
         end
 
         ikko_data["table"] = table_string
         return @ikko["memory_report.html", ikko_data]
+      end
+
+      def node_row_string(all_data, run_data, node)
+        table_string = ""
+        row_string = @ikko["cell_template.html", {"data"=>node, "options"=>"ROWSPAN=3"}]
+        row_string << @ikko["cell_template.html", {"data"=>"Used", "options"=>"BGCOLOR=#DDDDDD"}]
+        run_data.stages.each do |stage|
+          row_string << write_stage(run_data, node, stage)
+        end
+        row_string << write_xmx(run_data, node)
+        table_string << @ikko["row_template.html", {"data"=>row_string}]
+          
+        row_string = @ikko["cell_template.html", {"data"=>"Avg", "options"=>"BGCOLOR=#BBBBBB"}]
+        run_data.stages.each do |stage|
+          row_string << @ikko["cell_template.html", {"data"=>"#{average(all_data, stage, node)} MB", "options"=>"BGCOLOR=#BBBBBB"}]
+        end
+        table_string << @ikko["row_template.html", {"data"=>row_string}]
+
+        row_string = @ikko["cell_template.html", {"data"=>"Free", "options"=>"BGCOLOR=#999999"}]
+        run_data.stages.each do |stage|
+          row_string << write_free(run_data, node, stage)
+        end
+        table_string << @ikko["row_template.html", {"data"=>row_string}]
+        return table_string
+      end
+
+      def write_stage(run_data, node, stage)
+        str = ""
+        if (run_data[stage, node].nil?) then
+          str = @ikko["cell_template.html", {"data"=>"NODE NOT PRESENT", "options"=>"BGCOLOR=#ff0000"}]
+        else
+          str = @ikko["cell_template.html", {"data"=>"#{run_data[stage, node]} MB", "options"=>"BGCOLOR=#DDDDDD"}]
+        end
+        return str
+      end
+
+      def write_xmx(run_data, node)
+        str = ""
+        if run_data.xmx(node).nil? then
+          str = @ikko["cell_template.html", {"data"=>"NODE NOT PRESENT", "options"=>"BGCOLOR=#ff0000"}]
+        else
+          str = @ikko["cell_template.html", {"data"=>"#{run_data.xmx(node)} MB", "options"=>"BGCOLOR=#DDDDDD ROWSPAN=3"}]
+        end
+        return str   
+      end
+
+      def write_free(run_data, node, stage)
+        str = ""
+        if run_data.xmx(node).nil?  || run_data[stage, node].nil? then
+          str = @ikko["cell_template.html", {"data"=>"NODE NOT PRESENT", "options"=>"BGCOLOR=#ff0000"}]
+        else
+          str = @ikko["cell_template.html", {"data"=>"#{run_data.xmx(node) - run_data[stage, node]} MB", "options"=>"BGCOLOR=#999999"}]
+        end
+        return str
       end
 
       def create_avg_list(avg_files)
